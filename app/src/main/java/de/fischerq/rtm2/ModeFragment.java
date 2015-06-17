b7c4a2d414515fd8b81136c6d0429796b858aaae
Zera <fischerq@in.tum.de>
2015-06-17 11:16:08 +0200

    initial v2

---
+++ app/src/main/java/de/fischerq/rtm2/ModeFragment.java
@@ -0,0 +1,78 @@
+package de.fischerq.rtm2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModeFragment extends Fragment {
+    public static final String TAG = "mode";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_mode, container, false);
+
+        SeekBar speedSlider = (SeekBar) v.findViewById(R.id.speedSlider);
+        speedSlider.setProgress(10);
+        TextView speedDisplay = (TextView) v.findViewById(R.id.speedDisplay);
+        speedDisplay.setText("BPM: " + 160);
+
+        speedSlider.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                update(seekBar.getProgress());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+            void update(int position) {
+                int bpm = 160 + (int) (((double) position - 10) / 10 * 20);
+                TextView speedDisplay = (TextView) getView().findViewById(R.id.speedDisplay);
+                speedDisplay.setText("BPM: " + bpm);
+            }
+        });
+
+
+        Button buttonAdapt = (Button) v.findViewById(R.id.buttonAdapt);
+        buttonAdapt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Selected AdaptMode", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button buttonChallenge = (Button) v.findViewById(R.id.buttonChallenge);
+        buttonChallenge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Selected ChallengeMode", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+}
---
+++ app/src/main/java/de/fischerq/rtm2/NavigationDrawerCallbacks.java
@@ -0,0 +1,5 @@
+package de.fischerq.rtm2;
+
+public interface NavigationDrawerCallbacks {
+    void onNavigationDrawerItemSelected(int position);
+}
